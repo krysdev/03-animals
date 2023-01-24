77b7d958b4e1afcc7831fd65b20d4be76e7ba09c
KrysDev <webdevkrys@gmail.com>
2023-01-24 21:07:04 +0000

    x

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 // import 'bulma/css/bulma.css'; // don't assign any variable, just import
 
-import AnimalShow from "./AnimalShow";
+import { useState } from 'react';
+import AnimalShow from './AnimalShow';
 
 function App() {
-  const handleClick = () => { 
-    console.log('XXX')
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
   };
 
   return (
     <div>
       <button onClick={handleClick}>ADD</button>
-      <AnimalShow/>
+      <div>Count: {count}</div>
+      <AnimalShow />
     </div>
   );
 }
